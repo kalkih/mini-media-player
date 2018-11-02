31d158c8686d5e8affa0bd6387ed1680f73f216b
kalkih <mrkihlstrom@gmail.com>
2018-11-02 16:32:01 +0100

    Changed position of shuffle button when player is collapsed

--- mini-media-player.js
+++ mini-media-player.js
@@ -257,10 +257,10 @@ class MiniMediaPlayer extends LitElement {
       <div class='select flex'>
         ${active && config.hide_controls
           && !config.hide_volume ? this._renderVolControls(entity) : html``}
-        ${active && config.show_shuffle
-          && (config.hide_volume || config.hide_controls ) ? this._renderShuffle() : html``}
         ${active && config.hide_volume
           && !config.hide_controls ? this._renderMediaControls(entity) : html``}
+        ${active && config.show_shuffle
+          && (config.hide_volume || config.hide_controls ) ? this._renderShuffle() : html``}
         <div class='flex right'>
           ${config.show_source ? this._renderSource(entity) : html``}
           ${config.consider_idle_after ? this._renderIdleStatus() : html``}
