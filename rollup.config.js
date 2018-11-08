2fba25584228df2fb803d47f67240f6385227c36
Zhong Jianxin <azuwis@gmail.com>
2018-11-08 10:36:14 +0800

    Remove rollup-plugin-cleanup
    Comments are already removed by rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import cleanup from 'rollup-plugin-cleanup';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
@@ -15,7 +14,6 @@ export default {
       'https://unpkg.com/@polymer/lit-element@^0.6.2/lit-element.js?module': '@polymer/lit-element'
     }),
     resolve(),
-    cleanup({comments: 'none'}),
     terser()
   ]
 };
