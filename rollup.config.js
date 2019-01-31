6aab58b5e5486e9683977861e66cfa43b58c88c1
kalkih <mrkihlstrom@gmail.com>
2019-01-31 17:02:27 +0100

    Renamed main js file (#62)

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'mini-media-player.js',
+  input: 'main.js',
   output: {
     file: 'mini-media-player-bundle.js',
     format: 'umd',
