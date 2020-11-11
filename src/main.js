f4341b204967c01ef572edf430f3809b142d0990
Dan <59684116+dcs8@users.noreply.github.com>
2020-11-11 19:43:17 +0000

    :sparkles: Expose card to the Lovelace card picker (#436)

--- src/main.js
+++ src/main.js
@@ -444,3 +444,12 @@ class MiniMediaPlayer extends LitElement {
 }
 
 customElements.define('mini-media-player', MiniMediaPlayer);
+
+// Configures the preview in the Lovelace card picker
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type: 'mini-media-player',
+  name: 'Mini Media Player',
+  preview: false,
+  description: 'A minimalistic yet customizable media player card',
+});
