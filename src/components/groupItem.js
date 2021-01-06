a6dd263e67b8799e754eb8aed30b4fdfd3e0c238
Karl Kihlström <mrkihlstrom@gmail.com>
2021-01-06 17:16:39 +0100

    :lipstick: Change font-weight & opacity of speaker items

--- src/components/groupItem.js
+++ src/components/groupItem.js
@@ -22,10 +22,8 @@ class MiniMediaPlayerGroupItem extends LitElement {
         .disabled=${this.disabled}
         @change='${e => e.stopPropagation()}'
         @click='${this.handleClick}'>
-        <span>
           ${this.item.name}
-          ${this.master ? html`<span>(${t(this.hass, 'label.master')})</span>` : ''}
-        </span>
+          ${this.master ? html`<span class="master">(${t(this.hass, 'label.master')})</span>` : ''}
       </mmp-checkbox>
     `;
   }
@@ -44,20 +42,8 @@ class MiniMediaPlayerGroupItem extends LitElement {
 
   static get styles() {
     return css`
-      paper-checkbox {
-        padding: 8px 0;
-      }
-      paper-checkbox > span {
-        font-weight: 600;
-        text-transform: lowercase;
-      }
-
-      ha-card[artwork*='cover'][has-artwork] paper-checkbox[disabled] {
-        --paper-checkbox-checkmark-color: rgba(0,0,0,.5);
-      }
-      ha-card[artwork*='cover'][has-artwork] paper-checkbox {
-        --paper-checkbox-unchecked-color: #FFFFFF;
-        --paper-checkbox-label-color: #FFFFFF;
+      .master {
+        font-weight: 500;
       }
     `;
   }
