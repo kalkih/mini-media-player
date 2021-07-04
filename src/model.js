fb755333ab05b01fd2d940ef883b77d0cad13e89
Boomka <16853804+BoOmka@users.noreply.github.com>
2021-07-04 17:00:34 +0300

    Do not increment progress if player isn't playing (#521) #346
    * Do not increment progress if player isn't playing
    
    * Linter fix
    
    Co-authored-by: azhilin <zhilin.alx@gmail.com>

--- src/model.js
+++ src/model.js
@@ -164,7 +164,11 @@ export default class MediaPlayerObject {
   }
 
   get progress() {
-    return this.position + (Date.now() - new Date(this.updatedAt).getTime()) / 1000.0;
+    if (this.isPlaying) {
+      return this.position + (Date.now() - new Date(this.updatedAt).getTime()) / 1000.0;
+    } else {
+      return this.position;
+    }
   }
 
   get idleView() {
