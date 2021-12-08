ec8c3d956c986a0b7a3f5103d5133f2722083458
Alone <hi@anlo.ng>
2021-12-08 13:57:49 +0800

    Fix translation for ISO 639-1

--- src/utils/translation.js
+++ src/utils/translation.js
@@ -6,8 +6,10 @@ const getNestedProp = (obj, path) => path.split('.').reduce((p, c) => p && p[c] 
 
 const translation = (hass, label, hassLabel = undefined, fallback = 'unknown') => {
   const lang = hass.selectedLanguage || hass.language;
+  const l639 = lang.split('-')[0]
   return translations[lang] && getNestedProp(translations[lang], label)
     || hass.resources[lang] && hass.resources[lang][hassLabel]
+    || translations[l639] && getNestedProp(translations[l639], label)
     || getNestedProp(translations[DEFAULT_LANG], label)
     || fallback;
 };
